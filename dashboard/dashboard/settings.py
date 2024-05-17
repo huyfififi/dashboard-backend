5406d5602845ce96162d4d334348ccb7de70d5f5
Kazuki Kijima <kazukiyoshida96@gmail.com>
2024-05-16 23:30:50 -0700

    Fix connections between db and backend containers

--- dashboard/dashboard/settings.py
+++ dashboard/dashboard/settings.py
@@ -72,11 +72,15 @@ WSGI_APPLICATION = "dashboard.wsgi.application"
 
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
-
+# TODO: Replace hard-coded values with env
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': "postgres",
+        'USER': "postgres",
+        'PASSWORD': "postgres",
+        'HOST': "db",
+        'PORT': "5432",
     }
 }
 
