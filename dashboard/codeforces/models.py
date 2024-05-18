6ac8254a1c6fabf4523707696a843c2fc12ccc4c
Kazuki Kijima <kazukiyoshida96@gmail.com>
2024-05-17 17:17:11 -0700

    Add a new model for codeforces

--- dashboard/codeforces/models.py
+++ dashboard/codeforces/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class CodeforcesUser(models.Model):
+    last_updated = models.DateTimeField(auto_now=True, editable=False)
+
+    handle = models.CharField(max_length=50, editable=False, unique=True)
+    rating = models.IntegerField(blank=True, null=True)
+    max_rating = models.IntegerField(blank=True, null=True)
--- dashboard/codeforces/tests.py
+++ dashboard/codeforces/tests.py
@@ -1,3 +1,39 @@
-from django.test import TestCase
+import pytest
+from django.db.utils import IntegrityError
 
-# Create your tests here.
+from codeforces.models import CodeforcesUser
+
+
+@pytest.mark.django_db
+class TestCodeforcesUserModel:
+    def test_user_creation(self):
+        user = CodeforcesUser.objects.create(
+            handle="test_handle",
+            rating=1500,
+            max_rating=1600
+        )
+        retrieved_user = CodeforcesUser.objects.get(handle="test_handle")
+        assert retrieved_user.handle == "test_handle"
+        assert retrieved_user.rating == 1500
+        assert retrieved_user.max_rating == 1600
+        assert retrieved_user.last_updated is not None
+
+    def test_handle_is_unique(self):
+        CodeforcesUser.objects.create(
+            handle="test_handle",
+            rating=1500,
+            max_rating=1600
+        )
+        with pytest.raises(IntegrityError):
+            CodeforcesUser.objects.create(
+                handle="test_handle",
+                rating=1400,
+                max_rating=1500
+            )
+
+    def test_blank_fields(self):
+        user = CodeforcesUser.objects.create(
+            handle="new_handle"
+        )
+        assert user.rating is None
+        assert user.max_rating is None
