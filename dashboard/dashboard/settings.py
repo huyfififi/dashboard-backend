0e781e30a72cab35ba3f1fe73bd1c6ba6b7a24c4
Kazuki Kijima <kazukiyoshida96@gmail.com>
2024-05-17 17:44:49 -0700

    Add CI to run tests

--- dashboard/dashboard/settings.py
+++ dashboard/dashboard/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -83,7 +84,7 @@ DATABASES = {
         'NAME': "postgres",
         'USER': "postgres",
         'PASSWORD': "postgres",
-        'HOST': "db",
+        'HOST': os.getenv("DATABASE_HOST", "db"),
         'PORT': "5432",
     }
 }
